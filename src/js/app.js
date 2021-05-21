67affa28944dbef3b965f9c1e813f147e74777d2
AlexGrin1 <a_grynevich@mail.ru>
2021-05-21 18:11:10 +0300

    refactor: add breadcrumbs

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,2 @@
 import "../styles/style.scss";
-import * as info from "./product_create.js";
 import * as nav from "./nav.js";
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,19 +1,12 @@
 import * as info from "./dataset.js";
-const navWrapper = document.querySelector(".nav_wrapper");
-const textBlock = document.querySelector(".text_block");
-const dataNav = info.info.nav;
-const pageText = info.info.page_text;
 
-dataNav.forEach((el) => {
-  const link = document.createElement("div");
-  link.textContent = el.text;
-  link.setAttribute("href", el.href);
-  link.className = "nav_link";
-  navWrapper.appendChild(link);
-});
-pageText.forEach((el) => {
-  const text = document.createElement(el.tag);
-  text.textContent = el.content;
-  text.className = "text_info";
-  textBlock.appendChild(text);
+const arrow = document.querySelectorAll(".title_block");
+const block = document.querySelectorAll(".block");
+
+arrow.forEach((el) => {
+  el.addEventListener("click", (event) => {
+    block.forEach((el) => {
+      el.classList.toggle("hidden");
+    });
+  });
 });
